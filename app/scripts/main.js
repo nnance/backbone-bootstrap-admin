1ca4ce9d2ffbeeaae0b12e77a3bc4076707df0bd
Nick Nance <nance.nick@gmail.com>
2014-05-07 07:09:42 -0500

    fully working menus

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,23 +6,30 @@ require.config({
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
+        },
+        metisMenu: {
+            deps: ['jquery']
         }
     },
     paths: {
         jquery: '../bower_components/jquery/dist/jquery',
         backbone: '../bower_components/backbone/backbone',
         underscore: '../bower_components/underscore/underscore',
-        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap'
+        bootstrap: '../bower_components/sass-bootstrap/dist/js/bootstrap',
+        metisMenu: '../bower_components/metisMenu/jquery.metisMenu'
     }
 });
 
 require([
     'backbone',
     'jquery',
+    'bootstrap',
+    'metisMenu',
     'views/blank'
-], function (Backbone, $, BlankView) {
+], function (Backbone, $, bootstrap, metisMenu, BlankView) {
     Backbone.history.start();
     var blank = new BlankView();
     $('#body').append(blank.el);
     blank.render();
+    $('#side-menu').metisMenu();
 });
